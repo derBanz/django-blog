e09e27fa771d714ea1437b9176b86554180b42e6
Sebastian on Mac <sebastian.banz@deliveryhero.com>
2021-10-19 11:32:33 +0200

    First working version.

--- myblog/myblog/urls.py
+++ myblog/myblog/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
+    path('blog/<int:id>/', views.blog_post, name='blog_post'),
     path('admin/', admin.site.urls),
 ]
