b7edc229412c5a0a2a8f82a3f9bf75abcf898a1b
Sebastian on Mac <sebastian.banz@deliveryhero.com>
2021-10-21 22:28:29 +0200

    Added requirements.txt

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -128,34 +128,40 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Markdownify
 
-MARKDOWNIFY = {
-    "default": {
-        "BLEACH": True,
-        "MARKDOWN_EXTENSIONS": [
-            'markdown.extensions.fenced_code',
-        ],
-        "STRIP": False,
-        "WHITELIST_TAGS": [
-            'a',
-            'abbr',
-            'acronym',
-            'b',
-            'blockquote',
-            'code',
-            'em',
-            'h1',
-            'h2',
-            'h3',
-            'h4',
-            'h5',
-            'h6',
-            'i',
-            'li',
-            'ol',
-            'p',
-            'pre',
-            'strong',
-            'ul',
-        ]
-    }
-}
+# MARKDOWNIFY = {
+#     "default": {
+#         "BLEACH": True,
+#         "MARKDOWN_EXTENSIONS": [
+#             'markdown.extensions.fenced_code',
+#         ],
+#         "STRIP": False,
+#         "WHITELIST_ATTRS": [
+#             'href',
+#             'src',
+#             'alt',
+#         ],
+#         "WHITELIST_TAGS": [
+#             'a',
+#             'abbr',
+#             'acronym',
+#             'b',
+#             'blockquote',
+#             'code',
+#             'em',
+#             'h1',
+#             'h2',
+#             'h3',
+#             'h4',
+#             'h5',
+#             'h6',
+#             'i',
+#             'img',
+#             'li',
+#             'ol',
+#             'p',
+#             'pre',
+#             'strong',
+#             'ul',
+#         ]
+#     }
+# }
